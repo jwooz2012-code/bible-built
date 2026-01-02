e6ddc112454d89feca2b4e244c2b6f1ea0db72b1
john woosley <jwooz2012@gmail.com>
2026-01-02 14:10:21 +0000

    File changes

--- src/components/bible/hooks/useProgressMutations.jsx
+++ src/components/bible/hooks/useProgressMutations.jsx
@@ -7,22 +7,34 @@ export function useProgressMutations() {
   const { isGuest, guestAPI } = useGuestMode();
 
   const createProgressMutation = useMutation({
-    mutationFn: (data) => base44.entities.BookProgress.create(data),
+    mutationFn: (data) => {
+      if (isGuest) return guestAPI.bookProgress.create(data);
+      return base44.entities.BookProgress.create(data);
+    },
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['bookProgress'] }),
   });
 
   const updateProgressMutation = useMutation({
-    mutationFn: ({ id, data }) => base44.entities.BookProgress.update(id, data),
+    mutationFn: ({ id, data }) => {
+      if (isGuest) return guestAPI.bookProgress.update(id, data);
+      return base44.entities.BookProgress.update(id, data);
+    },
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['bookProgress'] }),
   });
 
   const unlockAchievementMutation = useMutation({
-    mutationFn: (data) => base44.entities.Achievement.create(data),
+    mutationFn: (data) => {
+      if (isGuest) return guestAPI.achievement.create(data);
+      return base44.entities.Achievement.create(data);
+    },
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['achievements'] }),
   });
 
   const createBibleProgressMutation = useMutation({
-    mutationFn: (data) => base44.entities.BibleProgress.create(data),
+    mutationFn: (data) => {
+      if (isGuest) return guestAPI.bibleProgress.create(data);
+      return base44.entities.BibleProgress.create(data);
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['bibleProgress'] });
       queryClient.invalidateQueries({ queryKey: ['bookProgress'] });
@@ -30,7 +42,10 @@ export function useProgressMutations() {
   });
 
   const updateBibleProgressMutation = useMutation({
-    mutationFn: ({ id, data }) => base44.entities.BibleProgress.update(id, data),
+    mutationFn: ({ id, data }) => {
+      if (isGuest) return guestAPI.bibleProgress.update(id, data);
+      return base44.entities.BibleProgress.update(id, data);
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['bibleProgress'] });
       queryClient.invalidateQueries({ queryKey: ['bookProgress'] });
