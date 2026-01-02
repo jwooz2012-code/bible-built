2384ee7107de6108a27c183ac29c8cf3f6dd4762
john woosley <jwooz2012@gmail.com>
2026-01-02 21:35:05 +0000

    File changes

--- src/components/bible/useBookProgress.jsx
+++ src/components/bible/useBookProgress.jsx
@@ -2,22 +2,19 @@ import { useProgressData } from './hooks/useProgressData';
 import { useProgressMutations } from './hooks/useProgressMutations';
 import { useProgressStats } from './hooks/useProgressStats';
 import { useAchievements } from './hooks/useAchievements';
-import { useBibleProgressTracker } from './hooks/useBibleProgressTracker';
 import { useChapterActions } from './hooks/useChapterActions';
 import { useMarkBookComplete } from './hooks/useMarkBookComplete';
 
 export function useBookProgress() {
-  const { user, progressData, achievements, bibleProgress, isLoading } = useProgressData();
+  const { user, progressData, achievements, isLoading } = useProgressData();
 
   const {
     createProgressMutation,
     updateProgressMutation,
     unlockAchievementMutation,
-    createBibleProgressMutation,
-    updateBibleProgressMutation,
   } = useProgressMutations();
 
-  const { getProgressForBook, calculateStats } = useProgressStats(progressData, bibleProgress);
+  const { getProgressForBook, calculateStats } = useProgressStats(progressData);
 
   const { checkAchievements } = useAchievements(
     progressData,
@@ -27,38 +24,26 @@ export function useBookProgress() {
     user
   );
 
-  const { updateBibleProgressChapter } = useBibleProgressTracker(
-    bibleProgress,
-    user,
-    createBibleProgressMutation,
-    updateBibleProgressMutation
-  );
-
   const { toggleChapter, restartBook } = useChapterActions(
     user,
     progressData,
     getProgressForBook,
     createProgressMutation,
     updateProgressMutation,
-    updateBibleProgressChapter,
     checkAchievements
   );
 
   const { markBookComplete } = useMarkBookComplete(
     user,
-    bibleProgress,
     getProgressForBook,
     createProgressMutation,
     updateProgressMutation,
-    createBibleProgressMutation,
-    updateBibleProgressMutation,
     checkAchievements
   );
 
   return {
     progressData,
     achievements,
-    bibleProgress,
     isLoading,
     getProgressForBook,
     calculateStats,
