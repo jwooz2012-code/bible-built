70cb4f890768ff290ebae8b3814ba1bd9da86400
john woosley <jwooz2012@gmail.com>
2026-01-02 13:18:25 +0000

    File changes

--- src/pages/ReadingCalendar.jsx
+++ src/pages/ReadingCalendar.jsx
@@ -41,7 +41,10 @@ export default function ReadingCalendar() {
 
   const { data: readingLogs = [] } = useQuery({
     queryKey: ['readingLogs'],
-    queryFn: () => base44.entities.ReadingLog.list(),
+    queryFn: async () => {
+      const user = await base44.auth.me();
+      return base44.entities.ReadingLog.filter({ user_id: user.id });
+    },
   });
 
   const addLogMutation = useMutation({
