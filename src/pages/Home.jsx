3bc9c5d975508e1c53c9a00964a7e1833e86920d
john woosley <jwooz2012@gmail.com>
2026-01-30 22:25:23 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,6 +43,7 @@ export default function Home() {
   const [selectedBook, setSelectedBook] = useState(null);
   const [selectedTestamentFilter, setSelectedTestamentFilter] = useState('OT');
   const [planOpen, setPlanOpen] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const [planPreviewOpen, setPlanPreviewOpen] = useState(false);
   const [showWelcome, setShowWelcome] = useState(false);
 
@@ -319,7 +320,10 @@ export default function Home() {
               allTimeLogs={allTimeLogs}
               todayKey={today}
               userId={userId}
-              onOpenPlanModal={() => setPlanOpen(true)}
+              onOpenPlanModal={(isEditMode = false) => {
+                setEditMode(isEditMode);
+                setPlanOpen(true);
+              }}
               onOpenPlanPreview={() => setPlanPreviewOpen(true)}
               onDismissPrompt={handleDismissPrompt}
               showPrompt={showPrompt}
@@ -480,10 +484,14 @@ export default function Home() {
         {/* Plan Modal */}
         <PlanModal
         open={planOpen}
-        onClose={() => setPlanOpen(false)}
+        onClose={() => {
+          setPlanOpen(false);
+          setEditMode(false);
+        }}
         userId={userId}
         existingPlan={plan}
         logs={allTimeLogs}
+        editMode={editMode}
         />
 
         {/* Plan Preview Sheet */}
