e91e2f717f11b70367ecdd15dd309b66931555c1
john woosley <jwooz2012@gmail.com>
2026-03-23 10:19:32 +0000

    File changes

--- base44/functions/adminStats/entry.ts
+++ base44/functions/adminStats/entry.ts
@@ -14,8 +14,3 @@ Deno.serve(async (req) => {
   const logs = await base44.asServiceRole.entities.ReadingLog.filter({ 'data.dateKey': dateKey }, '-created_date', 5000);
   return Response.json({ count: logs.length, date: dateKey });
 });
-  }
-
-  const logs = await base44.asServiceRole.entities.ReadingLog.filter({ dateKey: req.url.includes('date=') ? new URL(req.url).searchParams.get('date') : '2026-03-22' });
-  return Response.json({ count: logs.length, date: '2026-03-22' });
-});
